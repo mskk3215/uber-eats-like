2f0ffdff539899da4ccec4e705b65810fe93621a
mskk3215 <m.kaaiweb@gmail.com>
2022-10-08 15:47:23 +0900

    routing add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # namespaceで名前空間を付与する。コントローラーをグルーピングし、urlにもその情報を付与。
+  # app/controllers/api/v1/というディレクトリに作成
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants do
+        resources :foods, only: %i[index]
+      end
+      resources :line_foods, only: %i[index create]
+      # 特定のリソースに対するルーティング設定
+      # line_foods/replaceというURLのリクエストが来たらline_foods_controllerのreplaceメソッドを呼ぶ
+      put "line_foods/replace", to:"line_foods#replace"
+      resources :orders, only: %i[create]
+    end
+  end
 end
